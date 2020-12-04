d3800f9542079491b1062960a5361d07cd862fa2
malateshgodi <75314295+malateshgodi@users.noreply.github.com>
2020-12-04 19:52:51 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("***********Made some changes and working good!***********");
+		System.out.println("***********I made chenges throug webhook!***********");
 	}
 }
