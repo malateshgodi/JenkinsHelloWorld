9a6a48df6c6c0ad1141672f47d9610da7dfaf180
malateshgodi <75314295+malateshgodi@users.noreply.github.com>
2020-12-04 20:07:06 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("***********Trying to intregrate jenkins to github through webhook***********");
+		System.out.println("***********Webhook success***********");
 	}
 }
